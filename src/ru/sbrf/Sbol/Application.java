7b8d02ca03e1ed3a4019ee12e40db9eca23c3c30
OlgaDzhumanova <dzhum-olga@yandex.ru>
2020-08-29 17:56:01 +0300

    NewProject

---
+++ src/ru/sbrf/Sbol/Application.java
@@ -0,0 +1,7 @@
+package ru.sbrf.Sbol;
+
+public class Application {
+    public void Pay(){
+
+    }
+}
---
+++ src/ru/sbrf/Sbol/User.java
@@ -0,0 +1,13 @@
+package ru.sbrf.Sbol;
+
+public class User {
+    private boolean AccountNumber;
+    private boolean IdUser;
+
+
+    interface UserIn {
+        void usertn (int numberPhone, int amount, boolean currency);
+    }
+
+   
+}
